2e3655c41dcd544c79f5f173308f6dafdbb233b4
caf-3 <tomas.caetano@ubi.co.mz>
2021-05-27 06:12:50 +0200

    prepare to deploy

--- php_actions/db_actions.php
+++ php_actions/db_actions.php
@@ -1,9 +1,9 @@
 <?php
 
-define('SERVERNAME', 'sql310.epizy.com');
-define('USERNAME', 'epiz_24701584');
-define('PASSWORD','awr2C6Yom04');
-define('DBNAME','epiz_24701584_loginsystem');
+define('SERVERNAME', '');
+define('USERNAME', '');
+define('PASSWORD','');
+define('DBNAME','');
 $con = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 if(!$con){
     die('Houve um erro ao conectar a base de dados '. mysqli_connect_error());
